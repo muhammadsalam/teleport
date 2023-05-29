f0f0827f5901ff52f93ac9c1d91a909a1365ff52
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-05-29 21:17:28 +0300

    button components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Button from "./components/Button/Button";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">test</header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Button.Hug variant="secondary">Button</Button.Hug>
+                <Button.Hug>Button</Button.Hug>
+                <Button.Default>Создать страницу</Button.Default>
+                <Button.Primary>Изменить</Button.Primary>
+                <Button.Secondary>Открыть</Button.Secondary>
+                <Button.SecondaryTwo>Создать папку</Button.SecondaryTwo>
+                <Button.Tertiary>Создать папку</Button.Tertiary>
+                <Button.Link>77.83.173.18</Button.Link>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,61 @@
+import './Button.scss';
+import { ReactComponent as PlusSVG } from '../../assets/images/icons/plus.svg';
+import { ReactComponent as PencilSVG } from '../../assets/images/icons/pencil.svg';
+import { ReactComponent as OpenSVG } from '../../assets/images/icons/open.svg';
+import { ReactComponent as CopySVG } from '../../assets/images/icons/copy.svg';
+
+const Button = {
+    Hug: ({ disabled = false, variant = 'red', children }) => {
+        return (
+            <button disabled={disabled} className={`button button--hug button--${variant}`}>{children}</button>
+        );
+    },
+    Default: ({ disabled = false, children }) => {
+        return (
+            <button disabled={disabled} className={`button button--fixed button--red`}>
+                <PlusSVG className='button_icon' />
+                {children}
+            </button>
+        )
+    },
+    Primary: ({ disabled = false, children }) => {
+        return (
+            <button disabled={disabled} className={`button button--fixed button--primary`}>
+                <PencilSVG className='button_icon' />
+                {children}
+            </button>
+        )
+    },
+    Secondary: ({ disabled = false, children }) => {
+        return (
+            <button disabled={disabled} className={`button button--fixed button--secondary button--secondary-open`}>
+                <OpenSVG className='button_icon' />
+                {children}
+            </button>
+        )
+    },
+    SecondaryTwo: ({ disabled = false, children }) => {
+        return (
+            <button disabled={disabled} className={`button button--fixed button--secondary`}>
+                <PlusSVG className='button_icon' />
+                {children}
+            </button>
+        )
+    },
+    Tertiary: ({ disabled = false, children }) => {
+        return (
+            <button disabled={disabled} className={`button button--fixed button--tertiary`}>
+                {children}
+            </button>
+        )
+    },
+    Link: ({ disabled = false, children }) => {
+        return (
+            <button disabled={disabled} className={`button button--fixed button--secondary button--link`}>
+                <CopySVG className='button_icon' />
+                {children}</button>
+        )
+    }
+}
+
+export default Button;
