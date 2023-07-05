c4d8b2608fd31e0e0b2566ef96f1e7e50de8af03
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-07-05 16:12:52 +0300

    FolderItem context menu

--- src/components/FolderItem/FolderItem.tsx
+++ src/components/FolderItem/FolderItem.tsx
@@ -1,31 +1,76 @@
 import { combineClassNames } from "~/utils/combineClassNames";
 import styles from "./FolderItem.module.scss";
+import { ReactComponent as PencilSVG } from "Icon/pencil.svg";
+import { ReactComponent as DeleteSVG } from "Icon/delete.svg";
 import { ReactComponent as FolderSVG } from "Icon/folder.svg";
-import { FC, useState } from "react";
-import { FolderItemProps } from "./FolderItem.types";
+import { FC, useRef, useState } from "react";
+import { FolderItemProps, MenuPosition } from "./FolderItem.types";
+import { useOutsideClick } from "~/hooks/useOutsideClick";
+import { CSSTransition } from "react-transition-group";
 
 const FolderItem: FC<FolderItemProps> = ({ count, text }) => {
     const [isActive, setIsActive] = useState<boolean>(false);
+    const [isMounted, setIsMounted] = useState<boolean>(false);
+    const [menuPosition, setMenuPosition] = useState<MenuPosition>({
+        top: 0,
+        left: 0,
+    });
 
     const handleActivating: () => void = () =>
         !isActive && setIsActive(!isActive);
 
+    const handleContext: () => void = () => {
+        setIsMounted(!isMounted);
+    };
+
+    const contextRef = useRef<HTMLDivElement>(null);
+
+    useOutsideClick(contextRef, handleContext);
+
     return (
-        <div
-            className={combineClassNames(
-                styles.folderItem,
-                isActive && styles.active
-            )}
-            onClick={handleActivating}
-        >
-            <div className={styles.folderItem_count}>{count}</div>
-            <div className={styles.folderItem_button}>
-                <span className={styles.folderItem_button_span}></span>
-                <span className={styles.folderItem_button_span}></span>
-                <span className={styles.folderItem_button_span}></span>
+        <div className={styles.folder}>
+            <div
+                className={combineClassNames(
+                    styles.folderItem,
+                    isActive && styles.active
+                )}
+                onClick={handleActivating}
+            >
+                <div className={styles.folderItem_count}>{count}</div>
+                <div
+                    className={styles.folderItem_button}
+                    onClick={handleContext}
+                >
+                    <span className={styles.folderItem_button_span}></span>
+                    <span className={styles.folderItem_button_span}></span>
+                    <span className={styles.folderItem_button_span}></span>
+                </div>
+                <FolderSVG className={styles.folderItem_icon} />
+                <p className={styles.folderItem_title}>{text}</p>
             </div>
-            <FolderSVG />
-            <p className={styles.folderItem_title}>{text}</p>
+            <CSSTransition
+                in={isMounted}
+                timeout={300}
+                nodeRef={contextRef}
+                classNames={{
+                    enterActive: styles.contextBlock__enter,
+                    enterDone: styles.contextBlock__done,
+                }}
+                unmountOnExit
+            >
+                <div className={styles.contextBlock} ref={contextRef}>
+                    <ul className={styles.context}>
+                        <li className={styles.context_item}>
+                            <PencilSVG className={styles.context_icon} />{" "}
+                            Редактировать папку
+                        </li>
+                        <li className={styles.context_item}>
+                            <DeleteSVG className={styles.context_icon} />{" "}
+                            Удалить папку
+                        </li>
+                    </ul>
+                </div>
+            </CSSTransition>
         </div>
     );
 };
--- src/components/FolderItem/FolderItem.types.ts
+++ src/components/FolderItem/FolderItem.types.ts
@@ -2,3 +2,8 @@ export interface FolderItemProps {
     count: number;
     text: string;
 }
+
+export type MenuPosition = {
+    top: number;
+    left: number;
+}
