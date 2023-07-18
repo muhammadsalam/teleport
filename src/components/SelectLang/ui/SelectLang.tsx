93657103a5bd97fa5ed128db4950f2b4ccc2dd64
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-07-18 21:26:01 +0300

    SelectLang component (un)mount animation

--- src/components/SelectLang/ui/SelectLang.tsx
+++ src/components/SelectLang/ui/SelectLang.tsx
@@ -3,6 +3,8 @@ import { ReactComponent as ArrowDown } from "../assets/common/arrow-bottom.svg";
 
 import { useOutsideClick } from "~/hooks/useOutsideClick";
 import { FC, useRef, useState } from "react";
+import { combineClassNames } from "~/utils/combineClassNames";
+import { CSSTransition } from "react-transition-group";
 
 export const SelectLang: FC = () => {
     const [value, setValue] = useState<string>("ru");
@@ -22,15 +24,43 @@ export const SelectLang: FC = () => {
 
     return (
         <div className={style.select}>
-            <div className={style.selected}>
+            <div
+                className={combineClassNames(
+                    style.selected,
+                    isMounted && style.selected__opened
+                )}
+                onClick={handleMount}
+            >
                 <img
                     className={style.selected_icon}
-                    src={`../assets/countries/${value}.svg`}
+                    src={require(`../assets/countries/${value}.svg`)}
                 />
                 <span className={style.selected_text}>{value}</span>
                 <ArrowDown />
             </div>
-            <div className={style.select_menu} ref={dropdownRef}></div>
+
+            <CSSTransition
+                in={isMounted}
+                timeout={300}
+                nodeRef={dropdownRef}
+                classNames={{
+                    enterActive: style.menu__enter_active,
+                    enterDone: style.menu__enter_done,
+                }}
+                unmountOnExit
+            >
+                <div className={style.menu} ref={dropdownRef}>
+                    <ul className={style.list}>
+                        <li className={style.listItem}>
+                            <img
+                                className={style.listItem_icon}
+                                src={require(`../assets/countries/${value}.svg`)}
+                            />
+                            <span className={style.listItem_text}>RU</span>
+                        </li>
+                    </ul>
+                </div>
+            </CSSTransition>
         </div>
     );
 };
