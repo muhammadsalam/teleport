065e930be444309fa3423bf5c2b59087f54c43b7
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-08-08 14:32:20 +0300

    auth page in pages

--- src/pages/auth/ui/auth.tsx
+++ src/pages/auth/ui/auth.tsx
@@ -1,51 +1,39 @@
-import { BtnDefault, BtnHug, BtnTertiary, Input, Logo } from "shared/ui";
+import { BtnHug, BtnTertiary, Input } from "shared/ui";
 import style from "./auth.module.css";
 import { FC } from "react";
 import { ReactComponent as AuthSVG } from "../assets/auth.svg";
+import { SplitView } from "widgets/split-view/ui/SplitView";
+import { handleFormSubmiting } from "widgets/split-view";
 
 export const Auth: FC = () => {
-    const handleFormSubmiting = (e: any) => {
+    const handleFormSubmit: handleFormSubmiting = (e) => {
         e.preventDefault();
     };
 
     return (
-        <div className={style.wrapper}>
-            <header className={style.header}>
-                <Logo />
-            </header>
-            <div className={style.left}>
-                <AuthSVG className={style.left_svg} />
-            </div>
-            <div className={style.right}>
-                <form className={style.form} onSubmit={handleFormSubmiting}>
-                    <div className={style.right_title}>Войти</div>
-                    <Input
-                        type="text"
-                        placeholder="Введите логин"
-                        label="Имя пользователя"
-                        className={style.input}
-                    />
-                    <Input
-                        type="password"
-                        placeholder="Введите пароль"
-                        label="Пароль"
-                        className={style.input}
-                    />
-                    <a className={style.recovery} href="#">
-                        Забыли пароль?
-                    </a>
-                    <BtnHug className={style.button} grow onClick={() => {}}>
-                        Войти
-                    </BtnHug>
-                    <BtnTertiary
-                        className={style.button}
-                        grow
-                        onClick={() => {}}
-                    >
-                        Создать Аккаунт
-                    </BtnTertiary>
-                </form>
-            </div>
-        </div>
+        <SplitView handleFormSubmiting={handleFormSubmit} Image={AuthSVG}>
+            <div className={style.title}>Войти</div>
+            <Input
+                type="text"
+                placeholder="Введите логин"
+                label="Имя пользователя"
+                className={style.input}
+            />
+            <Input
+                type="password"
+                placeholder="Введите пароль"
+                label="Пароль"
+                className={style.input}
+            />
+            <a className={style.recovery} href="#">
+                Забыли пароль?
+            </a>
+            <BtnHug className={style.button} grow onClick={() => {}}>
+                Войти
+            </BtnHug>
+            <BtnTertiary className={style.button} grow onClick={() => {}}>
+                Создать Аккаунт
+            </BtnTertiary>
+        </SplitView>
     );
 };
