17d93a9df6e1daa7411b940834c768d94654fe7e
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-08-08 03:17:54 +0300

    auth page

--- src/pages/auth/ui/auth.tsx
+++ src/pages/auth/ui/auth.tsx
@@ -1,9 +1,13 @@
-import { BtnDefault, BtnHug, Input, Logo } from "shared/ui";
+import { BtnDefault, BtnHug, BtnTertiary, Input, Logo } from "shared/ui";
 import style from "./auth.module.css";
 import { FC } from "react";
 import { ReactComponent as AuthSVG } from "../assets/auth.svg";
 
 export const Auth: FC = () => {
+    const handleFormSubmiting = (e: any) => {
+        e.preventDefault();
+    };
+
     return (
         <div className={style.wrapper}>
             <header className={style.header}>
@@ -13,21 +17,34 @@ export const Auth: FC = () => {
                 <AuthSVG className={style.left_svg} />
             </div>
             <div className={style.right}>
-                <div className={style.right_title}>Войти</div>
-                <Input
-                    type="text"
-                    placeholder="Введите логин"
-                    label="Имя пользователя"
-                />
-                <Input
-                    type="password"
-                    placeholder="Введите пароль"
-                    label="Пароль"
-                />
-                <a href="#">Забыли пароль?</a>
-                <BtnHug grow={true} onClick={() => {}}>
-                    Войти
-                </BtnHug>
+                <form className={style.form} onSubmit={handleFormSubmiting}>
+                    <div className={style.right_title}>Войти</div>
+                    <Input
+                        type="text"
+                        placeholder="Введите логин"
+                        label="Имя пользователя"
+                        className={style.input}
+                    />
+                    <Input
+                        type="password"
+                        placeholder="Введите пароль"
+                        label="Пароль"
+                        className={style.input}
+                    />
+                    <a className={style.recovery} href="#">
+                        Забыли пароль?
+                    </a>
+                    <BtnHug className={style.button} grow onClick={() => {}}>
+                        Войти
+                    </BtnHug>
+                    <BtnTertiary
+                        className={style.button}
+                        grow
+                        onClick={() => {}}
+                    >
+                        Создать Аккаунт
+                    </BtnTertiary>
+                </form>
             </div>
         </div>
     );
