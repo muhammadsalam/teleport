cf13efb46fa3bcf28153deb9e839356df395724f
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-08-10 12:12:33 +0300

    404 error page fixes

--- src/pages/error/404/ui.tsx
+++ src/pages/error/404/ui.tsx
@@ -1,25 +1,24 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { SplitView } from "widgets/split-view";
 import { ReactComponent as NotFoundSVG } from "./assets/404.svg";
 import style from "./ui.module.css";
 import { BtnHug } from "shared/ui";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const NotFoundError: FC = () => {
-    const navigate = useNavigate();
-
-    useEffect(() => navigate("/404"), []);
-
     return (
-        <SplitView handleFormSubmiting={() => {}} Image={NotFoundSVG}>
+        <SplitView
+            handleFormSubmiting={(e) => e.preventDefault()}
+            Image={NotFoundSVG}
+        >
             <h1 className={style.title}>Такой страницы не существует</h1>
             <p className={style.description}>
                 Страница, которую Вы запрашиваете еще не создана или, возможно,
                 была удалена. Если вы считаете, что это ошибочная ситуация,
                 сообщите, пожалуйста, разработчикам об ошибке.
             </p>
-            <Link to="/">
-                <BtnHug className={style.button} typeof="button">
+            <Link to="/" className={style.link}>
+                <BtnHug className={style.button} grow typeof="button">
                     Вернуться на главную
                 </BtnHug>
             </Link>
