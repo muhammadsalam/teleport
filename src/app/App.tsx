bc200346ab772842db22bf2c17d9d83406750455
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-08-10 14:54:40 +0300

    500 error image changed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,6 @@
 import { Auth } from "pages/auth";
+import { NotFoundError } from "pages/error";
+import { InternalServerError } from "pages/error/500";
 import { Recovery } from "pages/recovery";
 import { Register } from "pages/register";
 import { Route, Routes } from "react-router-dom";
@@ -7,6 +9,9 @@ function App() {
     return (
         <div className="App">
             <Routes>
+                <Route path="*" element={<NotFoundError />} />
+                <Route path="500" element={<InternalServerError />} />
+                <Route path="/" element={"чё по кайфу"} />
                 <Route path="/auth" element={<Auth />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/recovery" element={<Recovery />} />
