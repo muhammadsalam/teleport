48ae32fb7ba79e07339e55a95fa17efdaa528823
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-08-10 15:04:34 +0300

    auth, register, recovery now in access folder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,18 @@
-import { Auth } from "pages/auth";
-import { NotFoundError } from "pages/error";
-import { InternalServerError } from "pages/error/500";
-import { Recovery } from "pages/recovery";
-import { Register } from "pages/register";
+import { Auth, Recovery, Register } from "pages/access";
+import { InternalServerError, NotFoundError } from "pages/error";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="*" element={<NotFoundError />} />
-                <Route path="500" element={<InternalServerError />} />
                 <Route path="/" element={"чё по кайфу"} />
                 <Route path="/auth" element={<Auth />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/recovery" element={<Recovery />} />
+
+                <Route path="*" element={<NotFoundError />} />
+                <Route path="500" element={<InternalServerError />} />
             </Routes>
         </div>
     );
