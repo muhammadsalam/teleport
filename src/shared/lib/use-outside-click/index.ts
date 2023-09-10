e38ef67a027055f5071b9a3e608a6733a42a6e40
Muhammadsalam Musaev <muhammadsalam@list.ru>
2023-09-10 12:23:15 +0300

    ref check

--- src/shared/lib/use-outside-click/index.ts
+++ src/shared/lib/use-outside-click/index.ts
@@ -3,22 +3,19 @@ import { useEffect, RefObject } from "react";
 export const useOutsideClick = <T extends HTMLElement>(
     ref: RefObject<T>,
     callback: () => void,
-    secondaryStyle?: string
 ): void => {
     const handleClick = (event: MouseEvent) => {
-        const isOutsideClick = ref.current && !ref.current.contains(event.target as Node)
-        const hasSecondaryClass = secondaryStyle && (event.target as HTMLElement).closest(`.${secondaryStyle}`);
-
-        if (isOutsideClick && !hasSecondaryClass) {
+        const isOutsideClick = !ref.current?.contains(event.target as Node);
+        if (ref.current && isOutsideClick) {
             callback();
         }
     };
 
     useEffect(() => {
-        document.addEventListener("mousedown", handleClick);
+        document.addEventListener("click", handleClick, true);
 
         return () => {
-            document.removeEventListener("mousedown", handleClick);
+            document.removeEventListener("click", handleClick, true);
         };
     }, []);
 };
